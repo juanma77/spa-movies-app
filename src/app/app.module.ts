1a213e631bb6a02f8ca7886ddcfbac6af063b40f
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-08-11 15:48:55 -0500

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    MovieComponent,
+    CardsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
